7b80b689c608f2270a9cbb3ba9af51d2004bcf37
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-03 16:15:01 +0100

    completed step 12

--- directory.rb
+++ directory.rb
@@ -3,7 +3,26 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save students to file"
+  puts "4. Load students to file"
   puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else 
+      puts "Please enter a correct value"
+  end
 end
 
 def interactive_menu
@@ -31,6 +50,27 @@ def input_students
     puts "Which cohort are they in?"
     cohort = gets.chomp.to_s
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def print_header
